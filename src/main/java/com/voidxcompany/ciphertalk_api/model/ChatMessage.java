66898daff9ceae75550e060e21a9ee3fce5d064a
Junior Rodrigues <rodriguesx@proton.me>
2025-10-02 13:26:10 -0300

    adicionando redis hash e @id

--- src/main/java/com/voidxcompany/ciphertalk_api/model/ChatMessage.java
+++ src/main/java/com/voidxcompany/ciphertalk_api/model/ChatMessage.java
@@ -2,7 +2,10 @@ package com.voidxcompany.ciphertalk_api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 
 @Getter
@@ -10,19 +13,25 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class ChatMessage {
+@RedisHash("ChatMessage")
+public class ChatMessage implements Serializable {
+
+    @Id
+    @JsonProperty("message_id")
+    private String messageId;
 
     @JsonProperty("message")
     private String message;
-    @JsonProperty("message_id")
-    private String messageId;
+    
     @JsonProperty("sender")
     private Long sender;
+    
     @JsonProperty("timestamp")
     private Timestamp timestamp;
+    
     @JsonProperty("room_address")
     private String roomAddress;
+    
     @JsonProperty("type")
     private ChatMessageType type;
-
 }
