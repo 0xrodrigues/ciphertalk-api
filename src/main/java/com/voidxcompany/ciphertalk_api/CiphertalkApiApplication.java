dab9042fb9726f9200eaef35795f3bf3adf4b52a
Junior Rodrigues <rodriguesx@proton.me>
2025-10-17 22:57:52 -0300

    initial commit

---
+++ src/main/java/com/voidxcompany/ciphertalk_api/CiphertalkApiApplication.java
@@ -0,0 +1,13 @@
+package com.voidxcompany.ciphertalk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiphertalkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiphertalkApiApplication.class, args);
+	}
+
+}
